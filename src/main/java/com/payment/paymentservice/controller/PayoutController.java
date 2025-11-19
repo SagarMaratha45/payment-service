a1f38659fdd99f5c86a4e4cd043f8c659b18f58c
I528931 <sagar02@sap.com>
2025-11-19 15:59:56 +0530

    followed api versioning standard

--- src/main/java/com/payment/paymentservice/controller/PayoutController.java
+++ src/main/java/com/payment/paymentservice/controller/PayoutController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/payouts")
+@RequestMapping("/api/v1/payouts")
 @RequiredArgsConstructor
 public class PayoutController {
 
